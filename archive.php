f8f4ec31e7266e077cdf3e509143df88dfdc29cf
djwiseman10 <dan.wiseman@buildertrend.com>
2018-09-18 15:18:28 -0500

    End of 9th Tutorial

--- archive.php
+++ archive.php
@@ -30,43 +30,12 @@ if (have_posts()) :
 	?></h2>
 
 	<?php
-	while (have_posts()) : the_post(); ?>
+	while (have_posts()) : the_post();
 
-		<article class="post">
-			<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
-
-			<p class="post-info"> <?php the_time('F jS, Y'); ?> | by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | Posted in
-
-				<?php 
-
-				$categories = get_the_category();
-				$separator = ", ";
-				$output = '';
+		get_template_part('content');
 
 
-				if ($categories) {
-
-					foreach ($categories as $category) {
-
-						$output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>' . $separator;
-					
-					}
-
-					echo trim($output, $separator);
-				}
-
-				?>
-
-
-			</p>
-
-
-			<?php the_excerpt(); ?>
-		
-		</article>
-
-
-	<?php endwhile;
+	endwhile;
 
 	else :
 		echo '<p>No Content Found</p>';
